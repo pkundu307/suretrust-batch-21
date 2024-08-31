967d016c2c01faa6db21d89654dc9f9ca93fea97
pkundu307 <pkundu307@gmail.com>
2024-08-31 21:36:42 +0530

    steps added as comment

--- fst.js
+++ fst.js
@@ -2,3 +2,14 @@ console.log('hello');
 console.log("Vivek upadhyay ");
 console.log("Sure trust");
 console.log("batch- 21");
+
+
+
+//1st -> create a new repository in github
+//2nd -> open vs code and paste all the command 1 by 1 (git init , git set remote origin, git add . , git commit
+//,git push origin origin name)
+// in case of push code -> use push command 
+// in case of get code -> use pull command
+// while creating new branch -> git branch branchname
+// switch branch -> git checkout branchname
+// check changes -> git status (do this before push and after making the commit )
