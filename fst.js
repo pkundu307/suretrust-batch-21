8b73d92c8c569a829dd156207ffb1fdb80a6834a
Prasanna Kundu <87819535+pkundu307@users.noreply.github.com>
2024-08-31 21:40:36 +0530

    checking browser vs code

--- fst.js
+++ fst.js
@@ -14,3 +14,4 @@ console.log("batch- 21");
 // switch branch -> git checkout branchname
 // check changes -> git status (do this before push and after making the commit )
 // for cloning -> git clone gitrepoLink
+//now i am using vs code from browser , just for fun , ignore this
